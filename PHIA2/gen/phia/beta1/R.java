f1fd78b9a8d01efb0cd83490a72a9bf6ed955f42
tsbayford <tsbayford@gmail.com>
2013-09-28 06:48:17 +0200

    again

--- PHIA2/gen/phia/beta1/R.java
+++ PHIA2/gen/phia/beta1/R.java
@@ -27,18 +27,25 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int logo=0x7f020002;
     }
     public static final class id {
+        public static final int button1=0x7f070003;
         public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
-        public static final int imageView1=0x7f070003;
+        public static final int imageView1=0x7f070004;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int info=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int dummy_butto=0x7f050003;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
+        public static final int get_info=0x7f050005;
+        public static final int out_breaks=0x7f050006;
+        public static final int project_myself=0x7f050007;
+        public static final int report_outbreak=0x7f050004;
     }
     public static final class style {
         /** 
